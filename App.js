491c963b32fa6838df1f406a429cd91b8272d8cf
Alexander Skrabl <alexander@skrabl.de>
2017-07-23 00:24:04 +0100

    adding react-navigation

--- App.js
+++ App.js
@@ -2,18 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { StackNavigator } from 'react-navigation';
+
 import { HomeScreen } from './app/components/Home/index';
 import { GameScreen } from './app/components/Game/index';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      // <View style={styles.container}>
-        <HomeScreen />
-      // </View>
-    );
+const App = StackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Game: { screen: GameScreen }
+  },
+  {
+    headerMode: 'none'
   }
-}
+);
+
+export default App;
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       // <View style={styles.container}>
+//         <HomeScreen />
+//       // </View>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
